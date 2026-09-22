293a27716500b323083ff3b41de9a4676c652d8b
DhuandharCoder <pt4597591@gmail.com>
2026-09-22 11:40:56 +0530

    .

---
+++ study_session.py
@@ -0,0 +1,19 @@
+#official documentation https://docs.opencv.org/5.0/
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    success, frame = camera.read()
+
+    if not success:
+        print("Could not access camera")
+        break
+
+    cv2.imshow("Study Session", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
